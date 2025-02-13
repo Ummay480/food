a46397bcc73ca12eb0725ef7c09f3b14708d92b9
Ummay <ummay480@gmail.com>
2025-02-13 14:12:38 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function Home() {
     return(
-        <h1>API</h1>
+        <h1 className="text-4xl bg-transparent text-orange-500">API</h1>
     )
  
 }
